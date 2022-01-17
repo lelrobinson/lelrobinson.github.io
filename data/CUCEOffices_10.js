c1bad2173c3b13e615f64c1c315439588ea8314c
lelrobinson <68024538+lelrobinson@users.noreply.github.com>
2022-01-17 18:06:41 -0500

    Add files via upload

---
+++ data/CUCEOffices_10.js
@@ -0,0 +1,12 @@
+var json_CUCEOffices_10 = {
+"type": "FeatureCollection",
+"name": "CUCEOffices_10",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "office": "Manhattan Office", "address": "570 Lexington Ave, 12th floor, NYC NY", "Latitude": 40.7571519, "Longitude": -73.9726033 }, "geometry": { "type": "Point", "coordinates": [ -73.9726033, 40.7571519 ] } },
+{ "type": "Feature", "properties": { "office": "Queens Office", "address": "89-62 164th Street, Jamaica, NY", "Latitude": 40.7055396, "Longitude": -73.7966669 }, "geometry": { "type": "Point", "coordinates": [ -73.7966669, 40.7055396 ] } },
+{ "type": "Feature", "properties": { "office": "Bronx Office", "address": "445 East 148th Street, Bronx NY", "Latitude": 40.8147909, "Longitude": -73.916146 }, "geometry": { "type": "Point", "coordinates": [ -73.916146, 40.8147909 ] } },
+{ "type": "Feature", "properties": { "office": "Brooklyn Office", "address": "1346 President Street", "Latitude": 40.6674658, "Longitude": -73.9449372 }, "geometry": { "type": "Point", "coordinates": [ -73.9449372, 40.6674658 ] } },
+{ "type": "Feature", "properties": { "office": "Urban Ag Office", "address": "Shirley Chisholm State Building, 55 Hanson Place, Brooklyn", "Latitude": 40.685516, "Longitude": -73.975627 }, "geometry": { "type": "Point", "coordinates": [ -73.975627, 40.685516 ] } }
+]
+}
---
+++ data/FEMAVACCINATIONSITES_16.js
@@ -0,0 +1,11 @@
+var json_FEMAVACCINATIONSITES_16 = {
+"type": "FeatureCollection",
+"name": "FEMAVACCINATIONSITES_16",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Name": "Church of God of East New York", "Address": "913 Sutter Ave, Brooklyn, NY 11207", "Latitude": 40.6711451, "Longitude": -73.8848467, "Info": " Wed and Thur evenings; Sat and Sun" }, "geometry": { "type": "Point", "coordinates": [ -73.8848467, 40.6711451 ] } },
+{ "type": "Feature", "properties": { "Name": "New Jerusalem Worship Center ", "Address": "12205 Smith St, Jamaica, NY 11434", "Latitude": 40.6797519, "Longitude": -73.7781905, "Info": "Tues and Thur evenings (tentative); Sat and Sun" }, "geometry": { "type": "Point", "coordinates": [ -73.7781905, 40.6797519 ] } },
+{ "type": "Feature", "properties": { "Name": "Convent Ave Baptist Church Tues and Wed evenings", "Address": " 420 W 145th St, New York, NY 10031", "Latitude": 40.824318, "Longitude": -73.9460393, "Info": "Sat and Sun" }, "geometry": { "type": "Point", "coordinates": [ -73.9460393, 40.824318 ] } },
+{ "type": "Feature", "properties": { "Name": "Fort Washington Collegiate Church", "Address": "729 W 181st St, New York, NY 10033", "Latitude": 40.8509998, "Longitude": -73.9374976, "Info": "Wed and Thur evenings (tentative; starting next week); Sat and Sun" }, "geometry": { "type": "Point", "coordinates": [ -73.9374976, 40.8509998 ] } }
+]
+}
